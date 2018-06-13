498fe521f6e8806427afbe70050acbdf33bd882c
Unknown <gracehughes317@gmail.com>
2018-06-13 11:25:27 -0500

    lil thang

--- WhereFall/Program.cs
+++ WhereFall/Program.cs
@@ -6,7 +6,7 @@ namespace WhereFall
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Where ya finna b at");
         }
     }
 }
